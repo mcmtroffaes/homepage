f094148270238fd955c46ec5b5d19d03b0d40d6a
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2014-03-26 16:57:52 +0000

    Build script for hakyll.

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-rm -rf build
-rm -rf ../blowyourmindwithhaskell-gh-pages/*
-rm -rf ../blowyourmindwithhaskell-gh-pages/.buildinfo
-sphinx-build -d build/doctrees . ../blowyourmindwithhaskell-gh-pages/
+ghc --make site.hs
+./site rebuild
